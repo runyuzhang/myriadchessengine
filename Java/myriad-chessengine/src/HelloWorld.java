1dcb39b6d1059014e1bde0427c665eca367729bf
Runyu Zhang <runyuzhang@berkeley.com>
2011-05-27 20:39:57 +0000

    Initialization with HelloWorld.java added

---
+++ Java/myriad-chessengine/src/HelloWorld.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author David Jeong
+ * To make changes to the file, go to your Eclipse and at the top, add the
+ * SVN Repository view (with the appropriate SVN information filled in) and
+ * then right click on the SVN source and "Check out" the project so that
+ * a copy of it will be added to your workspace. After you make changes, 
+ * right click on the project and go to Team->Synchronize. This will give you
+ * the conflicting changes that might exist and after you dismiss or accept
+ * them, finalize the synchronization and the repo in the SVN server will 
+ * be updated with your (and others') changes. 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println ("Hello World!");
+	}
+
+}
