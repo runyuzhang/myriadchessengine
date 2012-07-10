abdd99e8dfcd1f4cfdbe47426b239c73ebf9cb64
Runyu Zhang <runyuzhang@berkeley.com>
2012-07-10 15:53:30 +0000

    Here's a try at PVS. By all appearances it looks to be worse than Negamax, in particular, it seems to always be 1 or 0 ply.
    But I'm bad at chess, so here's a game it played against Crafty 23.4-6
    1. Nc3 c6 2. Nf3 b5 3. e4 Nf6 4. e5 g5 5. exf6 exf6 6. Bd3 d5 7. O-O h5 8. Re1+
    Be6 9. Nd4 Bd6 10. Bf5 Bxh2+ 11. Kxh2 Qe7 12. Nxe6 Qxe6 13. Bxe6 fxe6 14. Rxe6+
    Kf7 15. Qe2 Kg6 16. Qd3+ Kf7 17. Qf5 Kg7 18. Qxf6+ Kh7 19. Qg6# 1-0

--- Java/myriad-chessengine/src/tree/Pine.java
+++ Java/myriad-chessengine/src/tree/Pine.java
@@ -82,7 +82,7 @@ public class Pine {
 	}
 	private long eval(Position p, int color, int d) {
 		//Assume the position does end in someone winning, 
-		//and that this is the last level of search
+		//or that this is the last level of search
 		//d is the result of the game
 		
 		if (d == Position.WHITE_WINS) return (Long.MAX_VALUE-2)*color;
@@ -107,7 +107,83 @@ public class Pine {
 		return color * (mat + n_sq_w - n_sq_b + score);		
 	}
 	
-	public void PVS() {
+	/**
+	 * Starts NegaScout. When finished, we will know
+	 * the best move to make
+	 * @param original The current position of the board
+	 * @param prior_move The last moved played (by the opponent)
+	 * @param depth Search depth down the tree
+	 */
+	public void beginPVS(Position original,Move prior_move, int depth) {
+		System.out.println("PVS Start");
+		System.out.println("PVS Depth = " + depth);
+		Long time = System.nanoTime();
 		
+		counter = 0;
+		long best = Long.MIN_VALUE;
+		Maple[] children =root_leaf.getChildren();
+		if (children == null){
+			root_leaf.setChildren(original);
+			children = root_leaf.getChildren();
+		}		
+		for (Maple child: children) {
+			//It has to be negative because it's negascout??
+			long current = -PVS(child, original.makeMove(child.getPriorMove()), 
+					depth - 1, Long.MIN_VALUE, Long.MAX_VALUE); 
+			if (current > best) {
+				best_child = child;
+				best = current;
+			}
+		}
+		offsprings_of_best_child = best_child.getChildren();
+		System.out.println("Time Elapsed = " + (System.nanoTime()- time)/1000000);
+		System.out.println("Number of Positions Evaluated = " + counter);
+		System.out.println("PVS Done");
+	}	
+	
+	
+	//Converted code from Wikipedia that I don't understand
+	//Wikipedia says NegaScout = PVS. 
+	//Well, look at those negative signs...
+	//Well, PVS is shorter than Negascout.
+	//Omitting int color parameter since it seems the default is that it be 1...
+	/**
+	 * The recurrent method in our Negascout implementation
+	 * @param child The move which was made
+	 * @param p The position we evaluate, having just made child's move
+	 * @param depth Depth  down tree to search. Stops if depth is 0 
+	 * @param alpha Alpha-value
+	 * @param beta Beta-value
+	 * @return The worth of position p
+	 */
+	private long PVS(Maple child, Position p, int depth, long alpha, long beta) {
+		++counter;
+		Maple[] children = child.getChildren();
+		int outcome = p.getResult();
+		//Nothing more to search if this is a terminal node,
+		//we've reached infimum (lowest) depth,
+		//or the game has ended (in a victory or a draw)
+		if (children == null
+				|| depth == 0 || outcome !=Position.NO_RESULT ) {
+			return eval(p,1,outcome);
+		}
+		long b = beta;
+		for (Maple n : children) {
+			//p is the initial position. We get new positions by applying
+			//the moves in the Maple leaves
+			long score = -PVS(n, p.makeMove(n.getPriorMove()), 
+					depth - 1, -b, -alpha);
+			if (alpha < score && score < beta
+					&& children[0] != n) {
+				score = -PVS(n, p.makeMove(n.getPriorMove()),
+						depth -1, -beta, -alpha);
+			}
+			alpha = Math.max(alpha, score);
+			if (alpha >= beta) {
+				return alpha;
+			}
+			beta = alpha + 1;
+		}
+		return alpha;
 	}
 }
