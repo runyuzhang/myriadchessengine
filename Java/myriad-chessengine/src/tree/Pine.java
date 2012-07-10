f9a61a49569eb6e4a2f1f0de5383d49481c36490
Runyu Zhang <runyuzhang@berkeley.com>
2012-07-10 13:18:09 +0000

    Made a separate eval method. Removed the stasis of the private Negamax method.

--- Java/myriad-chessengine/src/tree/Pine.java
+++ Java/myriad-chessengine/src/tree/Pine.java
@@ -56,30 +56,11 @@ public class Pine {
 		System.out.println("Number of Positions Evaluated = " + counter);
 		System.out.println("NegaMax Done");
 	}
-	private static long NegaMax(Maple child, Position p, int depth, long alpha, long beta, int color) {
+	private long NegaMax(Maple child, Position p, int depth, long alpha, long beta, int color) {
 		counter ++;
 		int d = p.getResult();
 		if (d != Position.NO_RESULT|| depth == 0) {
-			if (d == Position.WHITE_WINS) return (Long.MAX_VALUE-2)*color;
-			else if (d == Position.BLACK_WINS) return (Long.MIN_VALUE+2)*color;
-			else if (d == Position.DRAW) return 0; 
-			Lorenz z = new Lorenz(p);
-			long score = 0;
-			long mat = (z.get(Lorenz.WHITE_ABSOLUTE_MATERIAL) & Crescent.MATERIAL_MASK)
-					- (z.get(Lorenz.BLACK_ABSOLUTE_MATERIAL) & Crescent.MATERIAL_MASK);
-			long dyn = z.get(Lorenz.DYNAMICS);
-			long two_bishops = dyn & 7;
-			if (two_bishops == 5) score -= 20;
-			else if (two_bishops == 3) score += 20;
-			long w_sent = z.get(Lorenz.WHITE_SENTINELS), b_sent = z.get(Lorenz.BLACK_SENTINELS);
-			int n_sq_w = 0, n_sq_b = 0;
-			for (int i = 0; i < 64; i ++){
-				if ((w_sent & 1) == 1) n_sq_w++;
-				else if ((b_sent & 1) == 1) n_sq_b++;
-				w_sent >>=1;
-				b_sent >>=1;
-			}
-			return color * (mat + n_sq_w - n_sq_b + score);
+			return eval(p, color, d);
 		} 
 		else {
 			Maple[] offsprings = child.getChildren();
@@ -98,5 +79,35 @@ public class Pine {
 	}
 	public Move getBestMove(){
 		return best_child.getPriorMove();
+	}
+	private long eval(Position p, int color, int d) {
+		//Assume the position does end in someone winning, 
+		//and that this is the last level of search
+		//d is the result of the game
+		
+		if (d == Position.WHITE_WINS) return (Long.MAX_VALUE-2)*color;
+		else if (d == Position.BLACK_WINS) return (Long.MIN_VALUE+2)*color;
+		else if (d == Position.DRAW) return 0; 
+		Lorenz z = new Lorenz(p);
+		long score = 0;
+		long mat = (z.get(Lorenz.WHITE_ABSOLUTE_MATERIAL) & Crescent.MATERIAL_MASK)
+				- (z.get(Lorenz.BLACK_ABSOLUTE_MATERIAL) & Crescent.MATERIAL_MASK);
+		long dyn = z.get(Lorenz.DYNAMICS);
+		long two_bishops = dyn & 7;
+		if (two_bishops == 5) score -= 20;
+		else if (two_bishops == 3) score += 20;
+		long w_sent = z.get(Lorenz.WHITE_SENTINELS), b_sent = z.get(Lorenz.BLACK_SENTINELS);
+		int n_sq_w = 0, n_sq_b = 0;
+		for (int i = 0; i < 64; i ++){
+			if ((w_sent & 1) == 1) n_sq_w++;
+			else if ((b_sent & 1) == 1) n_sq_b++;
+			w_sent >>=1;
+			b_sent >>=1;
+		}
+		return color * (mat + n_sq_w - n_sq_b + score);		
+	}
+	
+	public void PVS() {
+		
 	}
 }
